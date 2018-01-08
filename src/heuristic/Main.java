3c3a02cc714e8db489569a6aad189a6c4a091d5e
WouterDuijn <wdn480@student.vu.nl>
2018-01-08 17:43:32 +0100

    Wrote little layout for program parser

--- src/heuristic/Main.java
+++ src/heuristic/Main.java
@@ -1,5 +1,28 @@
 package heuristic;
 
+import java.util.Scanner;
+import java.io.PrintStream;
+
 public class Main {
+	
+	public static final int NR_PLANES = 6;
+	public static final String INPUT_PATH = "C:\\workspace\\heuristic\\inp";
+	
+
+     Main() {
+    	 //Initialize variables
+                 
+     }
+     
+     void start() {
+    	 //Parse input data
+    	 Parser parser = new Parser(INPUT_PATH);
+     }
+     
+     public static void main(String[] argv) {
+
+         new Main().start();
+
+}
 
 }
--- src/heuristic/Parser.java
+++ src/heuristic/Parser.java
@@ -1,5 +1,15 @@
 package heuristic;
 
 public class Parser {
+	
+	String input_path;
+	
+	Parser(String input_path){
+		this.input_path = input_path;
+	}
+	
+	void Parse(){
+		//Parse the input data here
+	}
 
 }
