d843c410a6d02de094826573d92849183734e375
daphnemolenwijk <d.p.molenwijk@student.vu.nl>
2018-01-31 09:37:16 +0100

    visuals code with adjusted axis size for powerpoint.

--- doc/visuals.R
+++ doc/visuals.R
@@ -3,26 +3,46 @@ source("data.r")
 par(mfrow=c(2,2))
 
 #Total time: 2897 seconds = 57.9
-hist(random, main = "Histogram of Random Algorithm Profit", 
-     breaks = 15,xlim = c(7.8,9.2), xlab="Profit (Millions)")
+hist(random, main="", breaks = 15,
+     xlim = c(7.8,9.2), xlab="", ylab="", yaxt="n", xaxt="n")
+axis(2, cex.axis=1.25)
+axis(1, cex.axis=1.25)
+mtext("Frequency", side=2, line=2.2, cex=1.2)
+mtext("Profit (€mln)", side=1, line=2.2, cex=1.2)
+title(main=, "Histogram Random Algorithm Profit", cex.main=1.5)
 mean(random)
 max(random)
 
 #Total time: 333 seconds = 6.66
-hist(hillclimber, main = "Histogram of HillClimber Algorithm Profit", breaks = 15,
-     xlim = c(9.5,12), xlab="Profit (Millions)")
+hist(hillclimber, main = "", breaks = 15,
+     xlim = c(9.5,12), xlab="", ylab="", yaxt="n", xaxt="n")
+axis(2, cex.axis=1.25)
+axis(1, cex.axis=1.25)
+mtext("Frequency", side=2, line=2.2, cex=1.2)
+mtext("Profit (€mln)", side=1, line=2.2, cex=1.2)
+title(main=, "Histogram Hill Climber Profit", cex.main=1.5)
 mean(hillclimber)
 max(hillclimber)
 
 #Total time: 1109 seconds = 22.18
-hist(hillclimberrestart, main = "Histogram of HillClimber Restart Algorithm Profit", 
-     breaks = 15,xlim = c(10.4,11.8), xlab="Profit (Millions)")
+hist(hillclimberrestart, main = "", 
+     breaks = 15,xlim = c(10.4,11.8), xlab="", ylab="", yaxt="n", xaxt="n")
+axis(2, cex.axis=1.25)
+axis(1, cex.axis=1.25)
+mtext("Frequency", side=2, line=2.2, cex=1.2)
+mtext("Profit (€mln)", side=1, line=2.2, cex=1.2)
+title(main=, "Histogram Hill Climber Restart Profit", cex.main=1.5)
 mean(hillclimberrestart)
 max(hillclimberrestart)
 
 #Total time: 369 seconds = 7.38
-hist(simulated, main = "Histogram of Simulated Annealing Algorithm Profit", 
-     breaks = 15,xlim = c(10.4,11.8), xlab="Profit (Millions)")
+hist(simulated, main = "", 
+     breaks = 15,xlim = c(10.4,11.8), xlab="", ylab="", yaxt="n", xaxt="n")
+axis(2, cex.axis=1.25)
+axis(1, cex.axis=1.25)
+mtext("Frequency", side=2, line=2.2, cex=1.2)
+mtext("Profit (€mln)", side=1, line=2.2, cex=1.2)
+title(main=, "Histogram Simulated Annealing Profit", cex.main=1.5)
 mean(simulated)
 max(simulated)
 
