e6d0604325daa9373f8cc3b2281c89ba2348359e
daphnemolenwijk <d.p.molenwijk@student.vu.nl>
2018-01-31 12:46:54 +0100

    Added variance calculation to R code

--- doc/Best Hometown.R
+++ doc/Best Hometown.R
@@ -6,14 +6,17 @@ cities = c("Amsterdam","Athens","Barcelona","Berlin","Bucharast","Budapest",
            "Reykjavik","Rome","Sofia","Stockholm","Tallinn","Vienna","Warsaw")
 
 pvalues <- data.frame(matrix(NA, nrow = length(cities), ncol = length(cities)))
+variances <- numeric(length(cities))
 
 par(mfrow=c(2, 3))
 
 for(i in 1:length(cities)) {
   data <- read.table(paste("City",i-1, "total_iterations_40000_runs_50_randomroutes_100.txt", sep='_'))
   assign(cities[i], data)
+  variances[i] = var(eval(parse(text = cities[i]))$V1)
   hist(data$V1, main=paste("Histogram of Profits Homebase", cities[i], sep=" "), xlab="Profits (€mln)")
 }
+print(variances) #quite similar variances, thus assumptionfor wilcox.test holds.
 
 for(i in 1:length(cities)) {
   city1 <- cities[i]
